36191206723ee752598f8c359e763a74d6e0e158
gulp+webpack <37628562+yaodong5063@users.noreply.github.com>
2022-08-05 20:45:33 +0800

    Add files via upload

---
+++ .umirc.ts
@@ -0,0 +1,29 @@
+export default {
+  npmClient: 'yarn',
+  // vite:{}
+  // extraBabelIncludes:['demoModule'],
+  cssLoaderModules:{
+    auto:true,
+  },
+  // chainWebpack(memo, { env, webpack }) {
+    // 设置 alias
+      // memo.module
+      // .rule('css')
+      // .oneOf('normal')
+      // .use('less')
+      //   .loader('less-loader')
+      //   .end()
+      // .end()
+      // .oneOf('normal1')
+      // .use('cssloader')
+      //   .loader('css-loader')
+      //   .options({
+      //     rules: {
+      //       auto: true
+      //     }
+      //   })
+      //   .end()
+
+    // console.log(memo.toString(),'config.toString();')
+  // },
+};
