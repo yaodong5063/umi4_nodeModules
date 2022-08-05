332c823e461ede69c1234135cad9b1ee9924bbb2
gulp+webpack <37628562+yaodong5063@users.noreply.github.com>
2022-08-05 20:39:07 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import yayJpg from "../assets/yay.jpg";
 import demo from "demoModule/index.js";
 import demoJsx from "demoModule/index.jsx";
+import styles from "./index.less";
 
 export default function HomePage() {
   demo();
   demoJsx();
+  console.log(styles,'styles')
+  
+  
   return (
     <div>
       <h2>Yay! Welcome to umi!</h2>
